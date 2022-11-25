7e2bc75ee7679f86cc722222b934e1d668302305
Juan Mari <jcelihueta@gipuzkoa.eus>
2022-11-25 12:14:52 +0100

    gpkg_sql: m_municipalities, azalerak multzokatuak

--- gpkg_sql.sh
+++ gpkg_sql.sh
@@ -12,21 +12,20 @@ declare -A idx_a
 # 1. m_municipalities (municipios) (carga: 1')
 des_a["m_municipalities"]="Udalerriak / Municipios / Municipalities"
 sql_a["m_municipalities"]="select
-b.idut idut,
 a.url_2d b5mcode,
+b.codmuni codmuni,
 a.nombre_e name_eu,
 a.nombre_c name_es,
-b.codmuni codmuni,
-c.comarca region,
 b.idnomcomarca codregion,
+c.comarca region,
 a.tipo_e type_eu,
 a.tipo_c type_es,
 a.tipo_i type_en,
-b.polygon geom
+sdo_aggr_union(sdoaggrtype(b.polygon,0.005)) geom
 from b5mweb_nombres.solr_gen_toponimia_2d a,b5mweb_25830.giputz b,b5mweb_nombres.n_municipios c
 where a.url_2d='M_'||b.codmuni
 and b.codmuni=c.codmuni
-and a.id_nombre1<>'996'"
+group by (a.url_2d,b.codmuni,a.nombre_e,a.nombre_c,b.idnomcomarca,c.comarca,a.tipo_e,a.tipo_c,a.tipo_i)"
 idx_a["m_municipalities"]="b5mcode"
 
 # 2. d_postaladdresses (direcciones postales) (carga: 4')
