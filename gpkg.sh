01f361376fff36264ccca14d6200b287f0eb090d
Juan Mari <jcelihueta@gipuzkoa.eus>
2023-01-27 12:18:00 +0100

    gpkg.sh: ermuak xehera eta gpkag-ak azkarrago sortu

--- gpkg.sh
+++ gpkg.sh
@@ -53,23 +53,46 @@ function hacer_gpkg {
 	then
 		sqlplus -s ${usu}/${pas}@${bd} <<-EOF2 > /dev/null
 		${or1_a["$nom"]}
-		
+
 		exit;
 		EOF2
 	fi
-	
-	# Geopackage
+
+	# Geopackage inicio
 	t="GIPUTZ"
-	rm "$fgpkg" 2> /dev/null
-	ogr2ogr -f "GPKG" -s_srs "EPSG:25830" -t_srs "EPSG:25830" -lco DESCRIPTION="${des_a["$nom"]}" "$fgpkg" OCI:${usu}/${pas}@${bd}:${t} -nln "$nom" -sql "${sql_a["$nom"]}" > /dev/null
-	ogrinfo -sql "create index ${nom}_idx1 on $nom (${idx_a["$nom"]})" "$fgpkg" > /dev/null
-	
+	rm "$fgpkg1" 2> /dev/null
+	ogr2ogr -f "GPKG" -s_srs "EPSG:25830" -t_srs "EPSG:25830" -lco DESCRIPTION="${des_a["$nom"]}" "$fgpkg1" OCI:${usu}/${pas}@${bd}:${t} -nln "$nom" -sql "${sql_a["$nom"]}" > /dev/null
+
+	# Renombrar campos
+	clist="$(ogrinfo -al -so "$fgpkg1" | gawk '
+	BEGIN {
+		a=0
+	}
+	{
+		if (a == 1) print substr($1, 1, length($1)-1)
+		if ($1 == "Geometry") a=1
+	}
+	')"
+	for c in $clist
+	do
+		c2="$(echo "$c" | gawk '{print tolower($0)}')"
+		ogrinfo -dialect ogrsql -sql "alter table $nom rename column $c to ${c}2" "$fgpkg1" > /dev/null
+		ogrinfo -dialect ogrsql -sql "alter table $nom rename column ${c}2 to $c2" "$fgpkg1" > /dev/null
+	done
+
+	# Crear índice
+	ogrinfo -sql "create index ${nom}_idx1 on $nom (${idx_a["$nom"]})" "$fgpkg1" > /dev/null
+	rm "$fgpkg2" 2> /dev/null
+	mv "$fgpkg1" "$fgpkg2" 2> /dev/null
+	rm "$fgpkg1" 2> /dev/null
+	# Geopackage fin
+
 	# Tareas Oracle 2
 	if [ "${or2_a["$nom"]}" != "" ]
 	then
 		sqlplus -s ${usu}/${pas}@${bd} <<-EOF2 > /dev/null
 		${or2_a["$nom"]}
-		
+
 		exit;
 		EOF2
 	fi
@@ -85,7 +108,7 @@ function copiar_gpkg {
 	EOF1
 	/usr/bin/sftp ${usup}@${hosp} <<-EOF1 > /dev/null 2> /dev/null
 	cd "$gpkgp"
-	put "$fgpkg" "$tmp"
+	put "$fgpkg2" "$tmp"
 	EOF1
 	/usr/bin/ssh ${usup}@${hosp} <<-EOF1 > /dev/null 2> /dev/null
 	cd "$gpkgp"
@@ -123,7 +146,8 @@ do
 	tip="${aconf[0]}"
 	nom="${aconf[1]}"
 	des="${aconf[2]}"
-	fgpkg="${gpkgd}/${nom}.gpkg"
+	fgpkg1="/tmp/${nom}.gpkg"
+	fgpkg2="${gpkgd}/${nom}.gpkg"
 	if [ $j -eq 0 ]
 	then
 		msg "0/${j}: $(date '+%Y-%m-%d %H:%M:%S') - No se hace nada"
