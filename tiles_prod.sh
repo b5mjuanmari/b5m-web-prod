114228b9a57c75f186d80079f4d14a8d279b32a8
Juan Mari <jcelihueta@gipuzkoa.eus>
2025-06-05 15:15:45 +0200

    tiles_prod.sh: lidar direktoriotik exekutatzeko optimizatu

--- tiles_prod.sh
+++ tiles_prod.sh
@@ -4,7 +4,7 @@
 TMP=/tmp; export TMP
 TMPDIR=$TMP; export TMPDIR
 ORACLE_HOME=/opt/oracle/instantclient; export ORACLE_HOME
-PATH=/opt/LAStools/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/opt/bin:/snap/bin:$ORACLE_HOME; export PATH
+export PATH="/opt/miniconda3/bin:/opt/LAStools/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:$ORACLE_HOME:/opt/bin:/snap/bin"
 export LANG=C
 export NLS_LANG=SPANISH_SPAIN.WE8ISO8859P15
 export NLS_NUMERIC_CHARACTERS=.,
@@ -13,7 +13,7 @@ LD_LIBRARY_PATH=$ORACLE_HOME/lib:/lib:/usr/lib; export LD_LIBRARY_PATH
 CLASSPATH=$ORACLE_HOME/JRE:$ORACLE_HOME/jlib:$ORACLE_HOME/rdbms/jlib; export CLASSPATH
 export PATH=.:$PATH
 
-export HOME="/home/juanmari"
+export HOME="/home/lidar"
 
 cd "${HOME}/SCRIPTS/WEB_PROD"
 
