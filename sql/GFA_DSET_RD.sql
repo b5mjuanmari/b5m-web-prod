94f1b2b19ac9caac2d9e262b5939f1e31e96313f
Juan Mari <jcelihueta@gipuzkoa.eus>
2025-06-11 14:16:21 +0200

    sql: datu multzoak, errepideen eta trenbideen hautaketa hobetu

--- sql/GFA_DSET_RD.sql
+++ sql/GFA_DSET_RD.sql
@@ -1,31 +1,29 @@
 select
   a.url_2d as b5mcode,
-  b.idut as b5midut,
+  c.idut as b5midut,
   case when a.nombre_e = a.nombre_c then a.nombre_e else a.nombre_e || ' / ' || a.nombre_c end as name,
   a.nombre_e as name_eu,
   a.nombre_c as name_es,
-  f.description_eu as type_eu,
-  f.description_es as type_es,
-  d.puente_tunel as bridge_tunnel,
-  d.descripcion_e description_eu,
-  d.descripcion_c description_es,
-  d.observacion_e observation_eu,
-  d.observacion_c observation_es,
+  e.description_eu as type_eu,
+  e.description_es as type_es,
+  b.puente_tunel as bridge_tunnel,
+  b.descripcion_e description_eu,
+  b.descripcion_c description_es,
+  b.observacion_e observation_eu,
+  b.observacion_c observation_es,
   c.polyline as geom
 from
   b5mweb_nombres.solr_gen_toponimia_2d a
 join
-  b5mweb_nombres.v_rel_vial_tramo b on to_char(b.idnombre) = a.id_nombre1
+  b5mweb_nombres.v_vialtramo b on b.idnombre = a.idnombre
 join
   b5mweb_25830.vialesind c on c.idut = b.idut
 join
-  b5mweb_nombres.v_vialtramo d on d.idut = b.idut
+  b5mweb_nombres.vv_color d on to_char(d.codigo) = a.id_nombre1
 join
-  b5mweb_nombres.vv_color e on to_char(e.codigo) = a.id_nombre1
-join
-  b5mweb_nombres.vv_color_desc f on f.color = e.color
+  b5mweb_nombres.vv_color_desc e on e.color = d.color
 where
   a.id_nombre2 = '9000'
-  and d.nomtipo_e = 'errepidea'
+  and b.nomtipo_e = 'errepidea'
 order by
   a.url_2d;
--- sql/GFA_DSET_RW.sql
+++ sql/GFA_DSET_RW.sql
@@ -1,21 +1,19 @@
 select
   a.url_2d as b5mcode,
-  b.idut as b5midut,
+  c.idut as b5midut,
   case when a.nombre_e = a.nombre_c then a.nombre_e else a.nombre_e || ' / ' || a.nombre_c end as name,
   a.nombre_e as name_eu,
   a.nombre_c as name_es,
-  d.puente_tunel as bridge_tunnel,
+  b.puente_tunel as bridge_tunnel,
   c.polyline as geom
 from
   b5mweb_nombres.solr_gen_toponimia_2d a
 join
-  b5mweb_nombres.v_rel_vial_tramo b on to_char(b.idnombre) = a.id_nombre1
+  b5mweb_nombres.v_vialtramo b on b.idnombre = a.idnombre
 join
   b5mweb_25830.vialesind c on c.idut = b.idut
-join
-  b5mweb_nombres.v_vialtramo d on d.idut = b.idut
 where
   a.id_nombre2 = '0990'
-  and d.nomtipo_e = 'trenbidea'
+  and b.nomtipo_e = 'trenbidea'
 order by
   a.url_2d;
