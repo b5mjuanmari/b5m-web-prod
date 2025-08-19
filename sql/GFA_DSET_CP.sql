f0e02ebb5b81f41142b255217404a69a434b6854
Juan Mari <jcelihueta@gipuzkoa.eus>
2025-08-19 16:44:47 +0200

    SQL fitxategiak: akats baten zuzenketa

--- sql/GFA_DSET_CP.sql
+++ sql/GFA_DSET_CP.sql
@@ -6,8 +6,8 @@ select herria as MUNI, kodea as CADASTRALZONINGCODE from GUNEAK_ZONAS
 
 -- HIRILUR_URBANO
 -- GFA_DST_CP_URBAN
-select erreferentz as NATIONALCADASTRALREFRERENCE, erreferentz as NAME, herria as MUNI, zona as CADASTRALZONINGCODE, concat('https://ssl6.gipuzkoa.eus/Catastro/map.htm?id=', substr(herria, 2), '&RefCat=', erreferentz) as URLINFO from HIRILUR_URBANO
+select erreferentz as NATIONALCADASTRALREFERENCE, erreferentz as NAME, herria as MUNI, zona as CADASTRALZONINGCODE, concat('https://ssl6.gipuzkoa.eus/Catastro/map.htm?id=', substr(herria, 2), '&RefCat=', erreferentz) as URLINFO from HIRILUR_URBANO
 
 -- LANDALUR_RUSTICA
 -- GFA_DST_CP_LAND
-select erreferentz as NATIONALCADASTRALREFRERENCE, erreferentz as NAME, herria as MUNI, concat('https://ssl6.gipuzkoa.eus/Catastro/map.htm?id=', substr(herria, 2), '&LRefCat=', erreferentz) as URLINFO from LANDALUR_RUSTICA
+select erreferentz as NATIONALCADASTRALREFERENCE, erreferentz as NAME, herria as MUNI, concat('https://ssl6.gipuzkoa.eus/Catastro/map.htm?id=', substr(herria, 2), '&LRefCat=', erreferentz) as URLINFO from LANDALUR_RUSTICA
