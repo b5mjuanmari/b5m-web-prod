7fc8d2dfc43a057ef02c0c693d46507b8afa07d2
Juan Mari <jcelihueta@gipuzkoa.eus>
2025-10-16 14:51:15 +0200

    sql: Katastroko erauzketa eguneratu

--- sql/GFA_DSET_CP.sql
+++ sql/GFA_DSET_CP.sql
@@ -1,13 +1,27 @@
-Catastro/Catastro_WMS
+-- multi
+-- Catastro/Catastro_WMS
 
 -- GUNEAK_ZONAS
 -- GFA_DST_CP_ZONING
-select herria as MUNI, kodea as CADASTRALZONINGCODE from GUNEAK_ZONAS
+select
+  herria as MUNI,
+  kodea as CADASTRALZONINGCODE
+from GUNEAK_ZONAS;
 
 -- HIRILUR_URBANO
 -- GFA_DST_CP_URBAN
-select erreferentz as NATIONALCADASTRALREFERENCE, erreferentz as NAME, herria as MUNI, zona as CADASTRALZONINGCODE, concat('https://ssl6.gipuzkoa.eus/Catastro/map.htm?id=', substr(herria, 2), '&RefCat=', erreferentz) as URLINFO from HIRILUR_URBANO
+select
+  erreferentz as NATIONALCADASTRALREFERENCE,
+  erreferentz as NAME,
+  herria as MUNI,
+  zona as CADASTRALZONINGCODE, concat('https://ssl6.gipuzkoa.eus/Catastro/map.htm?id=', substr(herria, 2), '&RefCat=', erreferentz) as URLINFO
+from HIRILUR_URBANO;
 
 -- LANDALUR_RUSTICA
 -- GFA_DST_CP_LAND
-select erreferentz as NATIONALCADASTRALREFERENCE, erreferentz as NAME, herria as MUNI, concat('https://ssl6.gipuzkoa.eus/Catastro/map.htm?id=', substr(herria, 2), '&LRefCat=', erreferentz) as URLINFO from LANDALUR_RUSTICA
+select
+  erreferentz as NATIONALCADASTRALREFERENCE,
+  erreferentz as NAME,
+  herria as MUNI,
+  concat('https://ssl6.gipuzkoa.eus/Catastro/map.htm?id=', substr(herria, 2), '&LRefCat=', erreferentz) as URLINFO
+from LANDALUR_RUSTICA;
