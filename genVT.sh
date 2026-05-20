c1f1069d6148536eb82a3cca2d1f83e5231e14ec
Juan Mari <jcelihueta@gipuzkoa.eus>
2026-05-20 12:01:21 +0200

    genVT: datos_explotacion ingurunean gorde

--- genVT.sh
+++ genVT.sh
@@ -2,7 +2,14 @@
 
 # Shapefile fitxategiak sortu VTerako (Vector Tiles)
 
-rm /tmp/landuse_merged.shp 2> /dev/null
+dir="/home/data/datos_explotacion/CUR/shape/EPSG_25830/Tiles"
+
+# Shapes
+landuse="${dir}/vt_landuse"
+
+
+# 1. landuse
+rm ${landuse}.* 2> /dev/null
 
 # Lehenik tileindex-etik shapefile guztiak batu
 for shp in /home9/SHP/fondo2005/fondo2005_*.shp; do
@@ -11,16 +18,16 @@ for shp in /home9/SHP/fondo2005/fondo2005_*.shp; do
     echo "Saltatzen: $shp"
     continue
   fi
-  if [ ! -f /tmp/landuse_merged.shp ]; then
+  if [ ! -f ${landuse}.shp ]; then
     ogr2ogr -f "ESRI Shapefile" \
       -where "Categoria LIKE 'Suelo e%' OR Categoria = 'Carreteras' OR Categoria = 'Autovia' OR Categoria = 'Viaductos y puentes' OR Categoria = 'Edificios'" \
-      /tmp/landuse_merged.shp \
+      ${landuse}.shp \
       "$shp"
   else
     ogr2ogr -f "ESRI Shapefile" \
       -update -append \
       -where "Categoria LIKE 'Suelo e%' OR Categoria = 'Carreteras' OR Categoria = 'Autovia' OR Categoria = 'Viaductos y puentes' OR Categoria = 'Edificios'" \
-      /tmp/landuse_merged.shp \
+      ${landuse}.shp \
       "$shp"
   fi
   echo "Eginda: $shp"
@@ -29,8 +36,8 @@ done
 # Ondoren fon_col1 gehitu
 ogr2ogr -f "ESRI Shapefile" \
   -update -append \
-  -where "TAG = 'cascos'" \
-  /tmp/landuse_merged.shp \
+  -sql "SELECT 'Cascos' AS Categoria FROM fon_col1 WHERE TAG = 'cascos'" \
+  ${landuse}.shp \
   /home5/SHP/Resto/fon_col1.shp
 echo "Eginda: fon_col1.shp"
 
