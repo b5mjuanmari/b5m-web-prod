9b13035c9c83b945fb2c00f401fbd8c7230a9544
Juan Mari <jcelihueta@gipuzkoa.eus>
2026-09-14 13:03:53 +0200

    tiles_prod: scripten ordena aldatu

--- tiles_prod.sh
+++ tiles_prod.sh
@@ -21,14 +21,15 @@ cd "${HOME}/SCRIPTS/WEB_PROD"
 ./genRotu.sh
 ./genVT.sh
 
+python3 genBuildings3D.py
+python3 vt_zentroideak.py /home/data/datos_explotacion/CUR/shape/EPSG_25830/Tiles
+
 # Toponimiaren aldaketak
 cd topo
 ./prozedura_guztiak.sh
 python3 99_copy_prod.py
 cd ..
 
-python3 genBuildings3D.py
-python3 vt_zentroideak.py /home/data/datos_explotacion/CUR/shape/EPSG_25830/Tiles
 python3 copy_tiles.py /home/data/datos_explotacion/CUR/shape/EPSG_25830/Tiles /home5/SHP
 
 exit 0
