6229e66235229edc08c6f18c64132ca62a2dfd04
Juan Mari <jcelihueta@gipuzkoa.eus>
2026-09-16 11:39:24 +0200

    tiles_prod: genBuildings3D ez da behar

--- tiles_prod.sh
+++ tiles_prod.sh
@@ -21,7 +21,7 @@ cd "${HOME}/SCRIPTS/WEB_PROD"
 ./genRotu.sh
 ./genVT.sh
 
-python3 genBuildings3D.py
+#python3 genBuildings3D.py
 python3 vt_zentroideak.py /home/data/datos_explotacion/CUR/shape/EPSG_25830/Tiles
 
 # Toponimiaren aldaketak
